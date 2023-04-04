ec37cbd328a54d21d491170941634e051bbfe7b2
ChoiSangwon <fdsa200@naver.com>
2023-04-04 11:24:37 +0900

    feat : mainscreen 각 클래스로 분할

--- lib/screens/infoscreen/mainscreen.dart
+++ lib/screens/infoscreen/mainscreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 class MainScreen extends StatefulWidget {
@@ -18,180 +17,232 @@ class _MainScreenState extends State<MainScreen> {
         // mainAxisAlignment: MainAxisAlignment.center,
         children: [
           //나에게 맞는 시설 추천
-          Container(
-            padding:
-                const EdgeInsets.only(top: 20, bottom: 20, left: 30, right: 30),
-            child: Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(right: 15),
-                  child: const Text(
-                    "복지피티",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                const Text(
-                  "한눈에 보는 복지정보",
-                  style: TextStyle(
-                    color: Color.fromRGBO(131, 131, 131, 1),
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return Dialog(
-                      child: SizedBox(
-                        height: 200,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(
-                              Icons.warning,
-                              size: 40,
-                            ),
-                            Text(
-                              '아직 서비스 준비중입니다',
-                              style: TextStyle(
-                                fontSize: 22,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  });
-            },
-            child: Stack(
-              children: [
-                Container(
-                  width: Get.width - 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(240, 241, 249, 1),
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.7),
-                        spreadRadius: 0,
-                        blurRadius: 5.0,
-                        offset:
-                            const Offset(2, 2), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 60,
-                  width: Get.width - 60,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        Icons.search,
-                        size: 40,
-                      ),
-                      Text(
-                        "나에게 맞는 복지서비스 찾기",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
+          RecommendBokji(),
           //테마별 보기
-          Container(
-            padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: const Text(
-                    "테마별 보기",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                Container(
-                  width: Get.width - 60,
-                  height: 160,
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(240, 249, 243, 1),
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.7),
-                        spreadRadius: 0,
-                        blurRadius: 5.0,
-                        offset:
-                            const Offset(2, 2), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: const Text(
-                    "최신 게시물",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                Container(
-                  width: Get.width - 60,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(249, 248, 240, 1),
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.7),
-                        spreadRadius: 0,
-                        blurRadius: 5.0,
-                        offset:
-                            const Offset(2, 2), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
+          ThemeBokji(),
+          //최신 게시물
+          communityBokji(),
         ],
       )),
+    );
+  }
+}
+
+class RecommendBokji extends StatefulWidget {
+  const RecommendBokji({super.key});
+
+  @override
+  State<RecommendBokji> createState() => _RecommendBokjiState();
+}
+
+class _RecommendBokjiState extends State<RecommendBokji> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding:
+              const EdgeInsets.only(top: 20, bottom: 20, left: 30, right: 30),
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(right: 15),
+                child: const Text(
+                  "복지피티",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const Text(
+                "한눈에 보는 복지정보",
+                style: TextStyle(
+                  color: Color.fromRGBO(131, 131, 131, 1),
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    child: SizedBox(
+                      height: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.warning,
+                            size: 40,
+                          ),
+                          Text(
+                            '아직 서비스 준비중입니다',
+                            style: TextStyle(
+                              fontSize: 22,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          },
+          child: Stack(
+            children: [
+              Container(
+                width: Get.width - 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(240, 241, 249, 1),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      spreadRadius: 0,
+                      blurRadius: 5.0,
+                      offset: const Offset(2, 2), // changes position of shadow
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 60,
+                width: Get.width - 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.search,
+                      size: 40,
+                    ),
+                    Text(
+                      "나에게 맞는 복지서비스 찾기",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ThemeBokji extends StatefulWidget {
+  const ThemeBokji({super.key});
+
+  @override
+  State<ThemeBokji> createState() => _ThemeBokjiState();
+}
+
+class _ThemeBokjiState extends State<ThemeBokji> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(bottom: 10),
+                child: const Text(
+                  "테마별 보기",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Container(
+                width: Get.width - 60,
+                height: 160,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(240, 249, 243, 1),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      spreadRadius: 0,
+                      blurRadius: 5.0,
+                      offset: const Offset(2, 2), // changes position of shadow
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class communityBokji extends StatefulWidget {
+  const communityBokji({super.key});
+
+  @override
+  State<communityBokji> createState() => _communityBokjiState();
+}
+
+class _communityBokjiState extends State<communityBokji> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(bottom: 10),
+                child: const Text(
+                  "최신 게시물",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Container(
+                width: Get.width - 60,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(249, 248, 240, 1),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      spreadRadius: 0,
+                      blurRadius: 5.0,
+                      offset: const Offset(2, 2), // changes position of shadow
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
