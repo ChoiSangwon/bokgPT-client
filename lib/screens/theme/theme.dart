132140a43665c4da4b8804f64602049945408ada
ChoiSangwon <fdsa200@naver.com>
2023-04-07 14:22:24 +0900

    feat : add theme dir & dart file init

---
+++ lib/screens/theme/theme.dart
@@ -0,0 +1,3 @@
+library theme;
+
+export 'themescreen.dart';
---
+++ lib/screens/theme/themescreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Themescreen extends StatefulWidget {
+  const Themescreen({super.key});
+
+  @override
+  State<Themescreen> createState() => _ThemescreenState();
+}
+
+class _ThemescreenState extends State<Themescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
