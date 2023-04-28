9ab7306a09da2fb7cee914f5cd32c23bfdc92529
ChoiSangwon <fdsa200@naver.com>
2023-04-28 23:43:51 +0900

    remove env.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:bokgpt_client/screens/infoscreen/infoscreen.dart';
+// import 'package:bokgpt_client/screens/infoscreen/infoscreen.dart';
+import 'package:bokgpt_client/screens/theme/themescreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               )),
-          body: MainScreen(),
+          body: Themescreen(),
         ));
   }
 }
